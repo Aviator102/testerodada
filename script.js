54fe25d4b079c954f67cd4f167ad5ee9f8ce36a4
Sonhador Brasil <magazinegravatai@gmail.com>
2024-10-24 02:24:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchAndDisplayResults() {
     try {
-        const response = await fetch('/api/fetchResults.php'); // Altere o caminho conforme necessário
+        const response = await fetch('/api/fetchResults.php');
         const results = await response.json();
 
         const resultadosDiv = document.getElementById('resultados');
@@ -24,29 +24,20 @@ async function fetchAndDisplayResults() {
 
 async function fetchAndSaveResults() {
     try {
-        console.log("Buscando resultados da API...");
         const response = await fetch('https://api-aviator-cb5db3cad4c0.herokuapp.com/history-odd?date=2024-09-18&numberVelas=10&betHouse=Aposta_ganha');
         
-        // Verifica se a resposta da API está ok
         if (!response.ok) {
             throw new Error(`Erro na API: ${response.status} ${response.statusText}`);
         }
 
-        const textResponse = await response.text(); // Lê a resposta como texto
-        console.log("Resposta da API:", textResponse); // Log da resposta
+        const data = await response.json();
 
-        const data = JSON.parse(textResponse); // Tenta fazer o parse do JSON
-
-        console.log("Resultados da API:", data); // Log dos resultados obtidos
-
-        const statusDiv = document.getElementById('status'); // Seleciona a div de status
+        const statusDiv = document.getElementById('status');
         statusDiv.innerHTML = ''; // Limpa o status anterior
 
         for (const item of data) {
             const valor = item.odd; // A propriedade "odd" contém o valor
             const hora = item.hour;  // A propriedade "hour" contém a hora
-
-            console.log(`Salvando resultado: Valor - ${valor}, Hora - ${hora}`);
 
             const saveResponse = await fetch('/api/saveResults.php', {
                 method: 'POST',
@@ -57,7 +48,6 @@ async function fetchAndSaveResults() {
             });
             
             const saveResult = await saveResponse.json();
-            console.log('Resultado do salvamento:', saveResult); // Verifica se o salvamento foi bem-sucedido
 
             // Atualiza a div de status com a mensagem recebida
             if (saveResult.status === 'success') {
